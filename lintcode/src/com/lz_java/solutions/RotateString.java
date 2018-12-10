1d26b4e4f3693113e3bfe614c48016250e769bb7
linzhen <273487984@qq.com>
2018-12-10 18:13:59 +0800

    lintcode

--- lintcode/src/com/lz_java/solutions/RotateString.java
+++ lintcode/src/com/lz_java/solutions/RotateString.java
@@ -6,15 +6,12 @@ public class RotateString {
         char[] cs = s.toCharArray();
         int offset = 2;
         int start = 0;
-        char tmp = cs[start];
-        int next = 5;
-        // fgabcde
+        char tmp = cs[5];
+        int next = 2;
         while(next != start) {
             cs[next] = tmp;
-            start = start<offset ? cs.length-offset+start : start-offset;
-            System.out.println(start);
-//            tmp = cs[start];
-//            next = start<cs.length-offset ? start+offset : start+offset-cs.length;
+            next = next<offset ? cs.length-offset+next : next-offset;
+            tmp = cs[next];
         }
         System.out.println(cs);
     }
